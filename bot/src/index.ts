5f485e9977ca998938a6834bb42d2657c73383dc
Arthur Duarte <tuiduarte@gmail.com>
2025-11-25 22:37:05 -0300

    feat: add pull_request.opened event

--- bot/src/index.ts
+++ bot/src/index.ts
@@ -7,6 +7,15 @@ export default (app: Probot) => {
     });
     await context.octokit.issues.createComment(issueComment);
   });
+
+  app.on("pull_request.opened", async (context) => {
+    app.log.info(`Pull request opened: ${context.payload.pull_request}`);
+    const pullRequestComment = context.issue({
+      body: "Thanks for opening this pull request!",
+    });
+    await context.octokit.issues.createComment(pullRequestComment);
+  });
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
