0c5be7238b380261e61597f7f493e7c630583659
raphaelmelo07 <raphaelmelogarcia@gmail.com>
2025-11-30 17:35:31 -0300

    fix: remove lib

--- bot/lib/types/infra/github/pullRequest.d.ts
+++
@@ -1,2 +0,0 @@
-import type { RestEndpointMethodTypes } from "@octokit/plugin-rest-endpoint-methods";
-export type PullRequestFile = RestEndpointMethodTypes["pulls"]["listFiles"]["response"]["data"][number];
--- bot/lib/utils/filenames.js
+++
@@ -1,2 +0,0 @@
-export const extractFilenames = (files) => files.map((file) => file.filename);
-//# sourceMappingURL=filenames.js.map
